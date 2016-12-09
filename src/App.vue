09392ffbe5633c284e19a528cefad8461a03d918
Dave Honneffer <pearofducks@gmail.com>
2016-12-09 21:24:16 +0100

    Initial

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Comp propMessage="foo" />
+</template>
+
+<script>
+import Comp from './Comp'
+
+export default {
+  name: 'app',
+  components: {
+    Comp
+  }
+}
+</script>
---
+++ src/Comp.js
@@ -0,0 +1,39 @@
+import Component from 'vue-class-component'
+
+@Component({
+  props: {
+    propMessage: String
+  },
+  template: `
+    <div>
+      <input v-model="msg">
+      <p>prop: {{propMessage}}</p>
+      <p>msg: {{msg}}</p>
+      <p>helloMsg: {{helloMsg}}</p>
+      <p>computed msg: {{computedMsg}}</p>
+      <button @click="greet">Greet</button>
+    </div>
+  `
+})
+export default class Comp {
+  // initial data
+  msg = 123
+
+  // use prop values for initial data
+  helloMsg = 'Hello, ' + this.propMessage
+
+  // lifecycle hook
+  mounted () {
+    this.greet()
+  }
+
+  // computed
+  get computedMsg () {
+    return 'computed ' + this.msg
+  }
+
+  // method
+  greet () {
+    alert('greeting: ' + this.msg)
+  }
+}
